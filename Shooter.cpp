faba31a781f02e6e1574af636b549b21811f7d3c
GHubensG <guidohubens@gmail.com>
2020-11-20 11:27:39 +0100

    main

--- Shooter.cpp
+++ Shooter.cpp
@@ -1,4 +1,29 @@
+int Input_S1;               //RPM from encoder MOTOR 1
+int Input_S2;               //RPM from encoder MOTOR 2
+int Safety_switch_1 = 0;    //Safety switch for the motors
+
+int Motor_01;               //OUT RPM Motor 1
+int Motor_02;               //OUT RPM Motor 2
+
+
+int Calculated_RPM;         //Calculated RPM;
+
+
+
+
+///////////////////////////setup/////////////////////////////////////
+    pinMode(Safety_switch_1, INPUT);
+    pinMode(Input_S1, INPUT);
+    pinMode(Input_S2, INPUT);
+    pinMode(Motor_01, OUTPUT);
+    pinMode(Motor_02, OUTPUT);
+
+
+
 void Shooter()
 {
-    
+
+
+
+
 }
