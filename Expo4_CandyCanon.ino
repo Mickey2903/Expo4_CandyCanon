4acf97b21cfc459a13d54f203c2b9be5b30dbce1
yannick1203 <49190149+yannick1203@users.noreply.github.com>
2020-11-20 10:32:54 +0100

    main

--- Expo4_CandyCanon.ino
+++ Expo4_CandyCanon.ino
@@ -1,9 +1,13 @@
 #include "Magazijn.h"
+#include "2DOFBASE.h"
+
+#define SERVOPINX 8;
+#define SERVOPINY 9;
 
 void setup()
 {
     Magazine.init();
-
+    2DOFBase CandyCanon(SERVOPINX, SERVOPINY);
 }
 
 void loop() 
